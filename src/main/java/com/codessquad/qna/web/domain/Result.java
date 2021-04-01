a4cab561c2be0bdbe4dca9d7efdb5d8c6d208d6b
rla36 <bhmarsd@naver.com>
2021-04-02 02:41:44 +0900

    실패 시 에러메세지를 전달할 Result 클래스 추가

---
+++ src/main/java/com/codessquad/qna/web/domain/Result.java
@@ -0,0 +1,24 @@
+package com.codessquad.qna.web.domain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+public class Result {
+    @JsonManagedReference
+    private boolean valid;
+
+    @JsonManagedReference
+    private String errorMessage;
+
+    private Result(boolean valid, String errorMessage) {
+        this.valid = valid;
+        this.errorMessage = errorMessage;
+    }
+
+    public static Result okay() {
+        return new Result(true, null);
+    }
+
+    public static Result fail(String errorMessage) {
+        return new Result(false, errorMessage);
+    }
+}
