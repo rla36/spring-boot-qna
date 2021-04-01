eaf787706eb0f637f3d347bec186c9add92c9258
rla36 <bhmarsd@naver.com>
2021-04-02 04:35:05 +0900

    swagger 사용 위한 의존성 추가

--- src/main/java/com/codessquad/qna/web/QnaApplication.java
+++ src/main/java/com/codessquad/qna/web/QnaApplication.java
@@ -2,17 +2,42 @@ package com.codessquad.qna.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.persistence.EntityListeners;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSwagger2
 public class QnaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QnaApplication.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("QnaApplication")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(PathSelectors.ant("/api/**"))    //api로시작하는 것들에 대한 문서화하겠다.
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("My Qna API")
+                .description("My Qna SLIPP API")
+                .version("2.0")
+                .build();
+    }
 }
