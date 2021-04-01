50339755337262e20609fe2a967b88bc0825e85c
rla36 <bhmarsd@naver.com>
2021-04-02 02:38:38 +0900

    질문 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO USER (id, user_id, password, name, email) VALUES (1, 'javajigi', 'test', '자바지기', 'javajigi@slipp.net');
 INSERT INTO USER (id, user_id, password, name, email) VALUES (2, 'sanjigi', 'test', '산지기', 'sanjigi@slipp.net');
 INSERT INTO USER (id, user_id, password, name, email) VALUES (3, 'user1', 'a', 'name1', 'email1@gmail.com');
+
+INSERT INTO QUESTION(id, writer_id, title, contents, written_date_time) VALUES(1, 1, '국내에서 ruby', 'ruby내용', '2020-10-10 17:36');
